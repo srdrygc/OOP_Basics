c768c6dbcc7a6e1efcf50ebbc19e7813788e345f
Serdar Yagci <srdrygc@gmail.com>
2018-08-15 11:02:39 -0500

    GetArea method added to Triangle class. It calculates area of triangle with given height and given length of base.

--- OOP_Basics/Triangle.cs
+++ OOP_Basics/Triangle.cs
@@ -6,5 +6,9 @@ namespace OOP_Basics
 {
     class Triangle
     {
+        public static double GetArea(double lengthOfBase, double height)
+        {
+            return lengthOfBase * height / 2;
+        }
     }
 }
